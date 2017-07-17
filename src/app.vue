5f1bf7b76b0d5898a3c5f58f0d3d18244621e016
sunhangye <sunhangye@chehejia.com>
2017-07-17 19:53:35 +0800

    sunhangye
    完成header部分

--- src/app.vue
+++ src/app.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <ele-header></ele-header>
+    <ele-header :seller="seller"></ele-header>
     <div class="tab">
       <div class="tab-item">
         <router-link to="/goods">商品</router-link>
@@ -17,9 +17,38 @@
 </template>
 
 <script>
+  import axios from 'axios'
   import header from './components/header/header.vue'
 
+  const OK = 0
   export default {
+    data () {
+      return {
+        seller: {}
+      }
+    },
+
+    created () {
+      this.$http.get('/api/seller')
+        .then(response => {
+          const result = response.body
+          if (result.code === OK) {
+            this.seller = result.data
+            console.log('vue-resouse', this.seller);
+          }
+        })
+      // 使用axios发送ajax请求mockjs提供的接口
+      axios.get('/api/seller')
+        .then(response => {
+          const result = response.data
+          if (result.code===OK) {
+            this.seller = result.data
+            this.seller.score = 3.6
+            console.log('axios', this.seller)
+          }
+        })
+    },
+
     components: {
       'ele-header': header
     }
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
+import VueResouce from 'vue-resource'
 import app from './app'
 import router from './router'
+import './mock/mockServer' // 引入加载模块
 import './common/stylus/fonts.styl'
 
+Vue.use(VueResouce)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
